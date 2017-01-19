1232c1d676bd4e878f1fac74a972e5d9b527b0a6
Flbl <Florian.Baletaud@Etudiant.univ-brest.fr>
2017-01-19 17:12:41 +0100

    Added files

--- Scripts/00_Environment_variables.R
+++ Scripts/00_Environment_variables.R
@@ -5,10 +5,11 @@ library(knitr)
 library(dismo)
 library(raster)
 library(rgdal)
+library(sp)
 library(ncdf4)
+library(tools)
 
-
-## @knitr readingData
+## @knitr temperature
 
 nc = nc_open(filename = "./Environment/global-analysis-forecast-phy-001-024.nc")
 # print(nc)
@@ -18,3 +19,30 @@ ncRas = raster("./Environment/global-analysis-forecast-phy-001-024.nc", varname 
 ncRas
 plot(ncRas, main = "Sea water potential temperature forecast (27-01-2017")
 
+
+
+## @knitr seafloor
+
+shps = dir("./Environment/global-seafloor-geomorphic-features-map", "*.shp")
+shps = file_path_sans_ext(shps)
+
+
+for(shp in shps) {
+  assign(shp, readOGR("./Environment/global-seafloor-geomorphic-features-map",layer = shp))
+}
+
+# shpv = as.vector(shps)
+# 
+# for(shpras in shp){
+#   shpras = raster(shp)
+#   res(shpras) = 1/12
+#   shpras = rasterize(shp, field = 1, shapras)
+# }
+
+Seamountsras = raster(Seamounts)
+res(Seamounts) = 1/12
+Seamountsras = rasterize(Seamounts, field = "Geomorphic", Seamountsras)
+
+plot(Seamounts, add = TRUE)
+plot(Rift_valleys, add = TRUE)         
+plot(Plateaus, add = TRUE)
