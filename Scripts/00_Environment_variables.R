359eb79d1e9a011ddba339fe63e505e97c29e045
Flbl <Florian.Baletaud@Etudiant.univ-brest.fr>
2017-01-20 16:00:04 +0100

    Updated blue habitats infos

--- Scripts/00_Environment_variables.R
+++ Scripts/00_Environment_variables.R
@@ -11,13 +11,13 @@ library(tools)
 
 ## @knitr temperature
 
-nc = nc_open(filename = "./Environment/global-analysis-forecast-phy-001-024.nc")
-# print(nc)
+nc = nc_open(filename = "./Environment/METOFFICE-GLO-SST-L4-NRT-OBS-SST-MON-V2_1484923518762.nc")
+print(nc)
 # summary(nc)
 
-ncRas = raster("./Environment/global-analysis-forecast-phy-001-024.nc", varname = "thetao")
+ncRas = raster("./Environment/METOFFICE-GLO-SST-L4-NRT-OBS-SST-MON-V2_1484923518762.nc", varname = "analysed_sst")
 ncRas
-plot(ncRas, main = "Sea water potential temperature forecast (27-01-2017")
+plot(ncRas, main = "Analysed Sea Surface Temperature")
 
 
 
@@ -31,6 +31,8 @@ for(shp in shps) {
   assign(shp, readOGR("./Environment/global-seafloor-geomorphic-features-map",layer = shp))
 }
 
+CoralShp = readOGR("./Environment/DataPack-14_001_WCMC008_CoralReef2010_v1_3/01_Data", layer = "14_001_WCMC008_CoralReef2010_v1_3")
+
 # shpv = as.vector(shps)
 # 
 # for(shpras in shp){
@@ -39,8 +41,10 @@ for(shp in shps) {
 #   shpras = rasterize(shp, field = 1, shapras)
 # }
 
+
+
 Seamountsras = raster(Seamounts)
-res(Seamounts) = 1/12
+res(Seamountsras) = 1/12
 Seamountsras = rasterize(Seamounts, field = "Geomorphic", Seamountsras)
 
 plot(Seamounts, add = TRUE)
