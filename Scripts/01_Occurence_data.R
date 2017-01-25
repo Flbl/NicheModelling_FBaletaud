713746c18a7a5b94b69de2a099419d3d156f6cbb
Flbl <Florian.Baletaud@Etudiant.univ-brest.fr>
2017-01-25 12:31:45 +0100

    QC flags working

--- Scripts/01_Occurence_data.R
+++ Scripts/01_Occurence_data.R
@@ -74,7 +74,10 @@ worldmap <- ggplot(world, aes(x=long, y=lat)) +
   theme(panel.background = element_rect(fill = "steelblue")) +
   coord_equal()
 
-occ_map = worldmap + geom_point(data = my_occs, aes(x = decimalLongitude, y = decimalLatitude), colour = "darkorange", shape = 21, alpha = 2/3)
+# Get the centroid of the whole coordinates
+centro <- data.frame(Long = mean(my_occs$decimalLongitude), Lat = mean(my_occs$decimalLatitude))
+
+occ_map = worldmap + geom_point(data = my_occs, aes(x = decimalLongitude, y = decimalLatitude), colour = "darkorange", shape = 21, alpha = 2/3) + geom_point(data = centro, aes(x = Long, y = Lat), colour = "red", shape = 21, alpha = 2/3)
 
 occ_map
 # occ_map + coord_map("sinusoidal")
@@ -114,6 +117,9 @@ table(my_occs$originalScientificName)
 
 # Filter by QCflags
 
+# See which flags are on
+as.logical(intToBits(my_occs$qc[1]))
+
 filter_by_QcFlags <- function(occ_dat, qc_var = "qc", qc_flags){
   get_allon_ids <- function(qc_var, qc_flags) {
     
@@ -136,7 +142,7 @@ filter_by_QcFlags <- function(occ_dat, qc_var = "qc", qc_flags){
   
   
   if(sum(c(8, 9, 20) %in% qc_flags) > 0){
-    stop("Flags 8, 9 and 20 are currently disabled and no records would be returned by your query",
+    stop("Flags 7,8,9,16,20,25,26,27,28 are currently disabled and no records would be returned by your query",
          call. = FALSE)
   }
   
@@ -150,13 +156,19 @@ filter_by_QcFlags <- function(occ_dat, qc_var = "qc", qc_flags){
   return(occ_dat)
 }
 
-my_occs_filt = filter_by_QcFlags(my_occs, qc_flags = c(1:7, 11:15))
+my_occs_filt = filter_by_QcFlags(my_occs, qc_flags = c(1:7,10:19,21:30))
 nrow(my_occs) - nrow(my_occs_filt)
 
-my_occs$qcnum <- qcflags(my_occs$qc, c(28))
-colors <- c("#ee3300", "#86b300")[my_occs$qcnum + 1]
+centrofilt = data.frame(Long = mean(my_occs_filt$decimalLongitude), Lat = mean(my_occs_filt$decimalLatitude))
 
-leaflet() %>%
-  addProviderTiles("CartoDB.Positron") %>%
-  addCircleMarkers(popup = paste0(my_occs$datasetName, "<br/>", my_occs$catalogNumber, "<br/><a href=\"http://beta.iobis.org/explore/#/dataset/", my_occs$resourceID, "\">OBIS dataset page</a>"), data = data.frame(lat = my_occs$decimalLatitude, lng = my_occs$decimalLongitude), radius = 3.5, weight = 0, fillColor = colors, fillOpacity = 1)
+  
+occ_map_filt = worldmap + geom_point(data = my_occs_filt, aes(x = decimalLongitude, y = decimalLatitude), colour = "darkorange", shape = 21, alpha = 2/3) + geom_point(data = centrofilt, aes(x = Long, y = Lat), colour = "red", shape = 21, alpha = 2/3)
+occ_map_filt
+
+# my_occs$qcnum = qcflags(my_occs$qc,c(1:7,10:19,21:30))
+# colors <- c("#ee3300", "#86b300")[my_occs$qcnum + 1]
+# 
+# leaflet() %>%
+#   addProviderTiles("CartoDB.Positron") %>%
+#   addCircleMarkers(popup = paste0(my_occs$datasetName, "<br/>", my_occs$catalogNumber, "<br/><a href=\"http://beta.iobis.org/explore/#/dataset/", my_occs$resourceID, "\">OBIS dataset page</a>"), data = data.frame(lat = my_occs$decimalLatitude, lng = my_occs$decimalLongitude), radius = 3.5, weight = 0, fillColor = colors, fillOpacity = 1)
 
