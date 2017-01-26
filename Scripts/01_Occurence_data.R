a1d7c13e04f0a3c05979c655e342ff0f8e3341da
Flbl <Florian.Baletaud@Etudiant.univ-brest.fr>
2017-01-26 17:51:04 +0100

    Update
    -Occurence data works and merges with NC dataset

--- Scripts/01_Occurence_data.R
+++ Scripts/01_Occurence_data.R
@@ -11,6 +11,7 @@ library(devtools)
 library(robis)
 library(leaflet)
 library(tibble)
+library(rgdal)
 
 library(ggmap)
 library(dplyr)
@@ -54,6 +55,11 @@ SpCheck<- function(checkSp){
   
 }
 
+# SpCheck("Carcharhinus amblyrhynchos")
+# SpCheck("Carcharhinus amblyrhinchos")
+# SpCheck("Galeolamna tufiensis")
+# SpCheck("Internecivus raptus") 
+
 OBISSP = function(my_sp, qc = c(1:7,10:19,21:30)){
   
 my_sp = SpCheck(my_sp)
@@ -65,31 +71,77 @@ my_sp_summ = checklist(my_sp)
 print(my_sp_summ$records)
 
 print("Getting data through Quality Flags :")
+
 my_occs = occurrence(my_sp, qc = qc)
+
+assign("my_occs", my_occs, .GlobalEnv)
 
 }
 
 
+
 OBISSP("Carcharhinus amblyrhynchos")
-OBISSP("Trianodon obesus")
+# OBISSP("Trianodon obesus")
+
+# Plotting
+leaflet() %>%
+  addProviderTiles("Esri.WorldImagery") %>%
+  addCircleMarkers(data = data.frame(lat = my_occs$decimalLatitude, lng = my_occs$decimalLongitude), radius = 3.5, weight = 0, fillColor = "orange", fillOpacity = 1)
 
 
+# @knitr Manualchk
 
 
+# Removing points not in the oceans (using ecoregions shapefile)
 
-SpCheck("Carcharhinus amblyrhynchos")
-SpCheck("Carcharhinus amblyrhinchos")
-SpCheck("Galeolamna tufiensis")
-SpCheck("qsd")
-my_sp <- "Carcharhinus amblyrhynchos"
+Ecoregion = readOGR(dsn = "./Environment/ecoregion/terEcorDissolved", layer = "terEcorDissolved")
 
-SpCheck(my_sp)
+my_occs_spatial <- SpatialPointsDataFrame(
+  coords = cbind(my_occs$decimalLongitude, my_occs$decimalLatitude),
+  data = my_occs,
+  proj4string = CRS("+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0"))
 
-my_sp_aphia <- get_wormsid(searchterm = my_sp, accepted = FALSE)
-my_sp_taxo <- worms_records(ids = my_sp_aphia, marine_only = TRUE)
-glimpse(my_sp_taxo) # Check status / valid_name / isMarine
+is_occ_Marine <- !(gIntersects(my_occs_spatial,Ecoregion, byid = TRUE))
+
+my_occs = my_occs[is_occ_Marine == TRUE, ]
+
+# Plotting for verification
+leaflet() %>%
+  addProviderTiles("Esri.WorldImagery") %>%
+  addCircleMarkers(data = data.frame(lat = my_occs$decimalLatitude, lng = my_occs$decimalLongitude), radius = 3.5, weight = 0, fillColor = "orange", fillOpacity = 1)
 
 
-# Getting occurences
+# Checking the datasets
 
-my_occs_new = occurrence(my_sp, qc = c(1:7,10:19,21:30))
+table(my_occs$datasetName)
+
+# Removing the National Taiwan Museum
+
+my_occs = my_occs[-which(my_occs$datasetName == "National Taiwan Museum"),]
+
+nrow(my_occs)
+
+# @knitr mergeOccs
+
+# Merging the occurrences from OBIS and New Caledonia dataset
+
+# Adding occurrence column to OBIS dataset
+my_occs$occurrence = 1
+
+## Reading/formatting the dataset
+NCRecords = read.table("./Biodiversity/dataset_fitted.csv", sep = ";", dec = ",", header = TRUE) 
+NC_occs <- NCRecords[,c(2,3,7)]
+NC_occs <- NC_occs[NC_occs$C_amblyrhynchos == "1",]
+colnames(NC_occs) <- c("decimalLatitude","decimalLongitude","occurrence")
+
+head(NC_occs)
+
+## merge OBIS and NC
+
+my_occs_merged <- merge(my_occs, NC_occs, all = TRUE)
+
+#Plot all the occurrences
+leaflet() %>%
+  addProviderTiles("Esri.WorldImagery") %>%
+  addCircleMarkers(data = data.frame(lat = my_occs_merged$decimalLatitude, lng = my_occs_merged$decimalLongitude), radius = 3.5, weight = 0, fillColor = "orange", fillOpacity = 1)
+
