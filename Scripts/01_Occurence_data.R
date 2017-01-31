b84cc0c242dad3f5959eb90755bd329c93da0103
Flbl <Florian.Baletaud@Etudiant.univ-brest.fr>
2017-01-31 18:26:32 +0100

    saving commit
    -Everything works
    -Need to turn all in function

--- Scripts/01_Occurence_data.R
+++ Scripts/01_Occurence_data.R
@@ -13,6 +13,7 @@ library(leaflet)
 library(tibble)
 library(rgdal)
 library(dplyr)
+library(sp)
 
 library(ggmap)
 library(dplyr)
@@ -110,7 +111,7 @@ NC_occs <- NCRecords[,c(2,3,7)]
 NC_occs <- NC_occs[NC_occs$C_amblyrhynchos >= "1",]
 colnames(NC_occs) <- c("decimalLatitude","decimalLongitude","individualCount")
 NC_occs$occurrence = 1
-
+NC_occs$datasetName <- "APEX"
 
 # head(NC_occs)
 
@@ -137,7 +138,7 @@ leaflet() %>%
 Occs = distinct(Occs, decimalLongitude, decimalLatitude, .keep_all = TRUE)
 
 
-# Double checking for points on earth using the bioclimatic grid
+# checking for points on earth using the bioclimatic grid
 
 ## Reading/creating the earth GRID
 earthGrid<-nc_open("./environment/temp/global-analysis-forecast-phy-001-024.nc") # Open any nc file from the bioclimatic data used
@@ -146,47 +147,79 @@ earthGrid<-nc_open("./environment/temp/global-analysis-forecast-phy-001-024.nc")
 earthGrid<-ncvar_get(earthGrid,"thetao")
 # temp<-as.data.frame(temp)
 
+
 earthGrid[(is.na(earthGrid))]<-1000
 earthGrid[(earthGrid<1000)]<-NA
 earthGrid[(earthGrid == 1000)]<-1
 earthGrid<-earthGrid[,order(c(1:ncol(earthGrid)),decreasing=T)] # Inverting coordinates to replace them right
 
-# plot(temp)
-
-earthGrid <- raster(nrows = 2041, ncols = 4320, xmn = -180, xmx = 179.9167, ymn = -80, ymx = 90, crs="+proj=longlat +datum=WGS84", vals = as.vector(earthGrid)) # Check the coordinates of the frame on copernicus
-writeRaster(earthGrid, "./environment/temp/earthGrid-1-12.asc",overwrite=T)
-
-earthGrid = raster("./environment/temp/couche-1-12.asc")
-
-## Gridding the occurrence dataset
+earthGrid <- raster(nrows = 2041, ncols = 4320, xmn = -180, xmx = 179.9167, ymn = -80, ymx = 90, crs="+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0", vals = as.vector(earthGrid)) # Check the coordinates of the frame on copernicus
+# writeRaster(earthGrid, "./environment/temp/earthGrid-1-12.asc",overwrite=T)
+# earthGrid = raster("./environment/temp/earthGrid-1-12.asc")
+# plot(earthGrid)
 
 
+## creating spatial points for our occurrences
+
+occsSpatial = SpatialPointsDataFrame(
+  coords = cbind(Occs$decimalLongitude, Occs$decimalLatitude),
+  data = Occs,
+  proj4string = CRS("+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0")
+)
+
+# plot(occsSpatial, add = TRUE)
+
+Occs <- Occs[-which(raster::extract(earthGrid,occsSpatial) == 1),]
+
+# writeOGR(occsSpatial, dsn = "./environment/temp", layer = "OccsSpatial302", driver = "ESRI Shapefile")
+
+# Reassign the rownames of Occs
+
+rownames(Occs) <- NULL
 
 
-## Multiplying the raster with the occurrence spatialpixelsdataframe
+# Double checking with the GSHHS "special islets" regions shapefile
+
+occsSpatial = SpatialPointsDataFrame(
+  coords = cbind(Occs$decimalLongitude, Occs$decimalLatitude),
+  data = Occs,
+  proj4string = CRS("+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0")
+)
+
+earthRegion <- readOGR(dsn = "./Environment/GSHHS_region", layer = "GSHHS_f_L1")
+
+is_occ_terrest <- gIntersects(occsSpatial,earthRegion, byid = TRUE, returnDense = FALSE)
+# is_occ_terrest_ids <- lapply(is_occ_terrest,function(x){
+#   
+#   cat(x,"\n")
+#   
+#   if(is.null(x)) return(NULL)
+#   
+#   id <- ((earthRegion@polygons)[[x]])@ID
+#   
+#   return(id)
+#   
+#   
+# })
 
 
+occ_terrest <- as.numeric(names(unlist(is_occ_terrest)))
+
+Occs <- Occs[-occ_terrest,]
+
+
+plot(earthGrid)
+plot(occsSpatial, add = TRUE)
+plot(occsSpatial[occ_terrest,], add = TRUE, col = "red")
 
 
 # Plotting for verification
 leaflet() %>%
   addProviderTiles("Esri.WorldImagery") %>%
-  addCircleMarkers(data = data.frame(lat = Occs$decimalLatitude[1718], lng = Occs$decimalLongitude[1718]), radius = 3.5, weight = 0, fillColor = "orange", fillOpacity = 1)
+  addCircleMarkers(data = data.frame(lat = Occs$decimalLatitude, lng = Occs$decimalLongitude), radius = 3.5, weight = 0, fillColor = "orange", fillOpacity = 1)
 
 
-# @knitr datasetname
 
-# Checking the datasets
-
-table(Occs$datasetName)
-
-# @knitr NTaiM
-
-# Removing the National Taiwan Museum
-
-Occs = Occs[-which(Occs$datasetName == "National Taiwan Museum"),]
-
-nrow(Occs)
 
 
 
