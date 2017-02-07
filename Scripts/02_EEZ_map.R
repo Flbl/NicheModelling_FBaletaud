58ef129b26a4cef7507ca160c466785cfbe7991a
Flbl <Florian.Baletaud@Etudiant.univ-brest.fr>
2017-02-07 14:13:47 +0100

    Added geomorphic features
    first steps towards the cropping of the shapefiles to the NC eez

--- Scripts/02_EEZ_map.R
+++ Scripts/02_EEZ_map.R
@@ -12,11 +12,13 @@ library(tibble)
 library(rgdal)
 library(dplyr)
 library(sp)
+library(tools)
+library(ncdf4)
+library(raster)
+library(rgeos)
 
 library(ggmap)
 library(dplyr)
-library(ncdf4)
-library(raster)
 library(stringr)
 library(readr)
 library(pryr)
@@ -24,10 +26,10 @@ library(marmap)
 library(lubridate)
 library(broom)
 library(mregions)
-library(rgeos)
 
 
-# Reading/creating the earth GRID a transformer en fonction
+
+# Reading/creating the earth GRID 
 
 earthGrid<-nc_open("./Environment/temp/earthgrid/global-analysis-forecast-phy-001-024.nc") # Open any nc file from the bioclimatic data used
 
@@ -35,18 +37,16 @@ earthGrid<-nc_open("./Environment/temp/earthgrid/global-analysis-forecast-phy-00
 earthGrid<-ncvar_get(earthGrid,"thetao")
 
 
-
 earthGrid[(is.na(earthGrid))]<-1000
 earthGrid[(earthGrid<1000)]<-NA
-earthGrid[(earthGrid == 1000)]<-1
-
-
+earthGrid[(earthGrid == 1000)]<-1 # 1 = earth
 
 earthGrid<-earthGrid[,order(c(1:ncol(earthGrid)),decreasing=T)] # basicly Inverting coordinates to replace them right
 
 earthGrid <- raster(nrows = 2041, ncols = 4320, xmn = -180, xmx = 179.9167, ymn = -80, ymx = 90, crs="+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0", vals = as.vector(earthGrid)) # Check the coordinates of the frame on copernicus
 
-earthGrid <- reclassify(earthGrid, cbind(NA,2))
+earthGrid <- reclassify(earthGrid, cbind(NA,2)) # 2 = ocean
+
 
 
 
@@ -60,13 +60,62 @@ eez[eez$PolygonID == 31,]
 eez[eez$PolygonID == 254,]
 
 eezNcPoly <- eez[which(eez$PolygonID == 254 | eez$PolygonID == 31),]
+
+# writeOGR(eezNcPoly, dsn = "./Miscellaneous", layer = "eezNcPoly", driver = "ESRI Shapefile")
+
+
 plot(earthGrid)
 plot(eezNcPoly, add = TRUE)
 
-# gintersect with only the New Caledonian polygons
+
+# intersect earthGrid with only the New Caledonian polygons
 eezNcGrid <- mask(earthGrid, eezNcPoly)
 eezNcGrid <- crop(eezNcGrid, extent(eezNcPoly))
 
+# writeRaster(eezNcGrid, "./Miscellaneous/eezNcGrid.asc", overwrite = TRUE)
+
 plot(eezNcGrid)
 plot(eezNcPoly, add = TRUE)
+
+
+
+# Crop the geomorphic substrate
+
+## @knitr seafloor
+
+
+
+shps = dir("./Environment/geomorph", "*.shp")
+shps = file_path_sans_ext(shps)
+
+
+cropNc <- function(shp, cropNc = eezNcPoly){
+  
+  shp <- assign(shp, readOGR("./Environment/geomorph",layer = shp))
+  
+  shp <- crop(shp, extent(cropNc))
+  
+  shp
+}
+
+
+croppedNcshp <- lapply(shps, cropNc)
+
+
+
+# Abyss <- cropNc("Abyss")
+# 
+# Abyssal_Classification <- cropNc("Abyssal_Classification")
+# 
+# Shelf_Classification <- cropNc("Shelf_Classification")
+# 
+# cropNc("Seamounts")
+# 
+# plot(Abyss)
+
+
+CoralShp = readOGR("./Environment/DataPack-14_001_WCMC008_CoralReef2010_v1_3/01_Data", layer = "14_001_WCMC008_CoralReef2010_v1_3")
+
+
+
 
