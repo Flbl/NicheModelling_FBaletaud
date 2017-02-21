bf23fae4560438fca87ee1e56aa0929bbb254fc1
Flbl <Florian.Baletaud@Etudiant.univ-brest.fr>
2017-02-21 12:02:05 +0100

    adding first glimpse of travel time script

---
+++ Scripts/05_Travel_Time.R
@@ -0,0 +1,51 @@
+# Reading/creating the earth GRID 
+
+earthGrid<-nc_open("./Environment/temp/earthgrid/global-analysis-forecast-phy-001-024.nc") # Open any nc file from the bioclimatic data used
+
+earthGrid<-ncvar_get(earthGrid,"thetao")
+
+earthGrid[(is.na(earthGrid))]<-1000
+earthGrid[(earthGrid<1000)]<-NA
+earthGrid[(earthGrid == 1000)]<-1 # 1 = earth
+
+earthGrid<-earthGrid[,order(c(1:ncol(earthGrid)),decreasing=T)] # basicly Inverting coordinates to replace them right
+
+earthGrid <- raster(nrows = 2041, ncols = 4320, xmn = -180, xmx = 179.9167, ymn = -80, ymx = 90, crs="+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0", vals = as.vector(earthGrid)) # Check the coordinates of the frame on copernicus
+
+earthGrid <- reclassify(earthGrid, cbind(NA,2)) # 2 = ocean
+
+# read the eez shapefile and crop the NC eez
+
+eez <- readOGR(dsn = "./Environment/World_EEZ_v9_20161021", layer = "eez")
+
+# The 2 polygons of interest representing the NC EEZ
+eez[eez$PolygonID == 31,]
+eez[eez$PolygonID == 254,]
+
+eezNcPoly <- eez[which(eez$PolygonID == 254 | eez$PolygonID == 31),]
+
+
+
+
+# intersect earthGrid with only the New Caledonian polygons
+
+eezNcGrid <- mask(earthGrid, eezNcPoly)
+eezNcGrid <- crop(eezNcGrid, extent(eezNcPoly))
+eezNcGrid <- reclassify(eezNcGrid, cbind(1,NA)) # NA = earth/outlimits
+eezNcGrid <- reclassify(eezNcGrid, cbind(2,1)) # 1 = Ocean
+
+
+
+
+
+# Transforming points coordinates to Behrman
+
+ncGridBehr <- projectRaster(eezNcGrid, 
+                            crs = CRS("+proj=cea +lon_0=0 +lat_ts=30 +x_0=0 +y_0=0 +datum=WGS84 +ellps=WGS84 +units=m +no_defs")
+                            , method = "ngb")
+
+plot(ncGridBehr)
+
+plot(eezNcGrid)
+
+
